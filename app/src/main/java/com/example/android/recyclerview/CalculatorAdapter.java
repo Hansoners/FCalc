eaa4c4cb03c7314065b4a7448604f44cd77f543f
Hansoners <hansonerschen@gmail.com>
2018-09-21 01:21:53 -0400

    Completed One Rep Max Calculator

--- app/src/main/java/com/example/android/recyclerview/CalculatorAdapter.java
+++ app/src/main/java/com/example/android/recyclerview/CalculatorAdapter.java
@@ -1,6 +1,8 @@
 package com.example.android.recyclerview;
 
+import android.app.LauncherActivity;
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -8,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -36,6 +39,18 @@ public class CalculatorAdapter extends RecyclerView.Adapter<CalculatorAdapter.Ca
         calculatorViewHolder.textViewTitle.setText(calculator.getTitle());
         calculatorViewHolder.textViewDesc.setText(calculator.getDescription());
         calculatorViewHolder.imageView.setImageDrawable(mCtx.getResources().getDrawable(calculator.getImage()));
+
+        final Calculator c = calculatorList.get(i);
+
+        calculatorViewHolder.imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (c.getId() == 1) {
+                    Intent intent = new Intent(v.getContext(), ORMActivity.class);
+                    v.getContext().startActivity(intent);
+                }
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/example/android/recyclerview/ORMActivity.java
+++ app/src/main/java/com/example/android/recyclerview/ORMActivity.java
@@ -1,13 +1,91 @@
 package com.example.android.recyclerview;
 
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class ORMActivity extends AppCompatActivity {
+
+public class ORMActivity extends AppCompatActivity implements TextWatcher {
+
+    TextInputEditText editText1;
+    TextInputEditText editText2;
+    TextView ormText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_orm);
+
+        editText1 = (TextInputEditText) findViewById(R.id.weightNum);
+        editText2 = (TextInputEditText) findViewById(R.id.repsNum);
+        ormText = (TextView) findViewById(R.id.ormNum);
+
+        editText1.addTextChangedListener(this);
+        editText2.addTextChangedListener(this);
+
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+        if (editText1.getText().hashCode() == editable.hashCode()) {
+            editText1.removeTextChangedListener(this);
+            float reps = 0;
+            float weight = 0;
+
+            if (!editText1.getText().toString().matches("")) {
+                weight = Integer.parseInt(editText1.getText().toString());
+            }
+
+            String stringValue = editText2.getText().toString();
+            if (!stringValue.matches("")) {
+                reps = Integer.parseInt(editText2.getText().toString());
+            }
+
+            float max = calculateMax(weight, reps);
+            ormText.setText("" + max);
+            editText1.addTextChangedListener(this);
+
+        } else if (editText2.getText().hashCode() == editable.hashCode()) {
+
+            editText2.removeTextChangedListener(this);
+            float reps = 0;
+            float weight = 0;
+
+            if (!editText2.getText().toString().matches("")) {
+                reps = Integer.parseInt(editText2.getText().toString());
+            }
+
+            String stringValue = editText1.getText().toString();
+            if (!stringValue.matches("")) {
+                weight = Integer.parseInt(editText1.getText().toString());
+            }
+            float max = calculateMax(weight, reps);
+            ormText.setText("" + max);
+            editText2.addTextChangedListener(this);
+        }
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+
+    public float calculateMax(float weight, float reps) {
+        return (weight * (1 + reps/30));
     }
 }
+
